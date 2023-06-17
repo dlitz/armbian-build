84cc7f7401c172709c004e12647a960eb9bddb63
Darsey Litzenberger <dlitz@dlitz.net>
2023-06-17 22:19:14 +0000

    Fix autoresizing rootfs when ROOTFS_TYPE=btrfs (**untested**) (XXX - Didn't work!)

--- extensions/fs-btrfs-support.sh
+++ extensions/fs-btrfs-support.sh
@@ -6,3 +6,9 @@ function add_host_dependencies__add_btrfs_tooling() {
 	display_alert "Adding BTRFS to host dependencies" "BTRFS" "debug"
 	EXTRA_BUILD_DEPS="${EXTRA_BUILD_DEPS} btrfs-progs" # @TODO: convert to array later
 }
+
+# Needed for auto-resizing the root filesystem
+function post_aggregate_packages__add_btrfsprogs_to_image() {
+	display_alert "Adding btrfs-progs to target dependencies" "BTRFS" "debug"
+	add_packages_to_image btrfs-progs
+}
